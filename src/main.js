73f57fde7aef032ee4ef06a45b3efede12697f64
Morgan LeeMaster <leemastermorgan@gmail.com>
2024-11-08 20:24:04 -0500

    Bounded orbit controls

--- src/main.js
+++ src/main.js
@@ -5,9 +5,9 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 // Set up the scene, camera, and renderer
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 1.5, 5); 
-const renderer = new THREE.WebGLRenderer({ antialias: true });
+const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 2000);
+camera.position.set(1.5,1.8,-1.9);
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -18,10 +18,10 @@ dracoLoader.setDecoderPath('/draco/');
 loader.setDRACOLoader(dracoLoader);
 
 // Lighting setup
-const light = new THREE.RectAreaLight(0x85939b, 0.25, 10, 20);
+const light = new THREE.RectAreaLight(0xffffff, 0.25, 10, 20);
 light.position.set(0, 2.85, 0);
 light.lookAt(0, 3, 0);
-const spotLight = new THREE.SpotLight(0x85939b, 9);
+const spotLight = new THREE.SpotLight(0xffffff, 9);
 spotLight.position.set(0, 2.85, 0);
 spotLight.angle = Math.PI; 
 spotLight.castShadow = true;
@@ -30,7 +30,7 @@ scene.add(light, spotLight);
 
 // Load environment model
 loader.load(
-    '/assets/models/EnvironmentFinal.glb',
+    '/assets/models/EnvironmentFinal5.glb',
     (gltf) => {
         scene.add(gltf.scene);
     },
@@ -42,17 +42,20 @@ loader.load(
     }
 );
 
-// Orbit controls
+// initialize orbit controls
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true; // Smooth the controls
-controls.dampingFactor = 0.05;
+controls.target.set(-1,.7,1);
 
-// Handle window resizing
-window.addEventListener('resize', () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-});
+// smooth controls
+controls.enableDamping = true;
+controls.dampingFactor = 0.01;
+controls.update();
+
+// bound orbit controls
+controls.minPolarAngle = Math.PI / 2.9;
+controls.maxPolarAngle = Math.PI / 2;
+controls.minAzimuthAngle = Math.PI / 1.3;
+controls.maxAzimuthAngle = -Math.PI * 1.2;
 
 // Animation loop
 function animate() {
